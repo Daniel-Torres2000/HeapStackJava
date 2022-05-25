58416118220ea086c16759d448cc868ba4b956fe
Daniel Torres <62557742+Daniel-Torres2000@users.noreply.github.com>
2022-05-25 00:51:56 -0600

    Add files via upload

---
+++ ProyectoHeapStack/src/Controller/Animal.java
@@ -0,0 +1,21 @@
+package Controller;
+public class Animal {
+    public String species, name;
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
