baf080fe499bff158552a6ebe27fc9b13cdcb698
Daniel Torres <62557742+Daniel-Torres2000@users.noreply.github.com>
2022-05-25 22:21:49 -0600

    Add files via upload

---
+++ ProyectoHeapStackVr2Completo/src/Controller/Principal.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import Vistas.FormPrincipal;
+
+public class Principal {
+    public static void main(String[] args) {
+        //clase 1 - main
+        FormPrincipal window = new FormPrincipal();
+        window.setVisible(true);
+    }
+}
